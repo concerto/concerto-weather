1f1a5c37a05cb151af15e0f721354cf6f94e56bd
Brian Michalski <bmichalski@gmail.com>
2013-03-19 20:53:40 -0700

    Add content building logic to create weather content.
    We add the build_content method to Weather which will get used automatically by the DynamicContent code to build the specific weather content we display.

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,6 +1,24 @@
 class Weather < DynamicContent
   DISPLAY_NAME = 'Weather'
 
+  def build_content
+    require 'rss'
+    require 'net/http'
+
+    url = "http://weather.yahooapis.com/forecastrss?p=#{self.config['zip_code']}"
+
+    feed = Net::HTTP.get_response(URI.parse(url)).body
+
+    rss = RSS::Parser.parse(feed, false, true)
+
+    weather_data = rss.items.first
+    sanitized_data = weather_data.description
+    htmltext = HtmlText.new()
+    htmltext.name = weather_data.title
+    htmltext.data = "<h1>#{rss.channel.title}</h1><i>#{weather_data.title}</i><p>#{sanitized_data}</p>"
+    return [htmltext]
+  end
+
   # Weather needs a location.
   def self.form_attributes
     attributes = super()
