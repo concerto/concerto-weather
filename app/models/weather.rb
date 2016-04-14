80fd8eda0029f4ba72d84561d28113f7ff56d23f
Gabe Perez <perez283@gmail.com>
2016-04-13 22:04:11 -0400

    Add ConcertoConfig for OpenWeatherMap API key. Add API request to weather model.

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -2,70 +2,43 @@ class Weather < DynamicContent
   DISPLAY_NAME = 'Weather'
 
   UNITS = {
-    'c' => 'Celsius',
-    'f' => 'Fahrenheit'
+    'metric' => 'Celsius',
+    'imperial' => 'Fahrenheit'
   }
 
-  validate :woeid_must_exist
-
   def build_content
-    require 'rss'
+    require 'json'
     require 'net/http'
 
-    url = "http://weather.yahooapis.com/forecastrss?w=#{self.config['woeid']}&u=#{self.config['units']}"
+    # Build request url 
+    params = {
+      q: self.config['location'],
+      u: self.config['units'],
+      cnt: 4,
+      mode: 'json',
+      appid: ConcertoConfig['open_weather_map_api_key']
+    }
 
-    feed = Net::HTTP.get_response(URI.parse(url)).body
+    url = "http://api.openweathermap.org/data/2.5/forecast/daily?#{params.to_query}"
 
-    rss = RSS::Parser.parse(feed, false, true)
+    # Make request to OpenWeatherMapAPI
+    response = Net::HTTP.get_response(URI.parse(url)).body
+    data = JSON.parse(response)
+    
+    # Build HTML using API data
+    rawhtml = ""
 
-    weather_data = rss.items.first
-    sanitized_data = weather_data.description
+    # Create HtmlText content
     htmltext = HtmlText.new()
-    htmltext.name = weather_data.title
-    rawhtml = "<h1>#{rss.channel.title}</h1><i>#{weather_data.title}</i><p>#{sanitized_data}</p>"
-    Rails.logger.debug rawhtml
+    htmltext.name = "Today's weather in #{data['city']['name']}"
     htmltext.data = ActionController::Base.helpers.sanitize(rawhtml, :tags => ['i', 'img', 'b', 'br', 'p', 'h1'])
+
     return [htmltext]
   end
 
   # Weather needs a location.  Also allow specification of units
   def self.form_attributes
     attributes = super()
-    attributes.concat([:config => [:woeid, :units]])
-  end
-
-  def woeid_must_exist
-    if !self.config.nil?  # had to add thius because rake dynamic_content:refresh was blowing up on nil object
-      if self.config['woeid'].empty?
-        errors.add(:woeid, 'must be specified')
-      else
-        data = []
-        #begin
-        woeid = URI.escape(self.config['woeid'])
-        url = URI.escape("http://query.yahooapis.com/v1/public/yql?q=select * from geo.places where woeid = #{woeid} limit 1&format=json")
-        uri = URI.parse(url)
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Get.new(uri.request_uri)
-        response = http.request(request)
-        if response.code == '200'  #ok
-          json = response.body
-          data = ActiveSupport::JSON.decode(json)
-        end
-        #rescue
-        #  Rails.logger.debug("Yahoo not reachable @ #{url}.")
-        #  return
-        #end
-        if data.empty? || data['query']['count'] == 0
-          errors.add(:woeid, 'not valid')
-        else
-          results = data['query']['results']['place']
-          info = results['name']
-          info = info + ', ' + results['admin1']['content'] if !results['admin1']['content'].empty? if !results['admin1'].nil?
-          info = info + ', ' + results['admin2']['content'] if !results['admin2']['content'].empty? if !results['admin2'].nil?
-          info = info + ', ' + results['country']['content'] if !results['country']['content'].empty? if !results['country'].nil?
-          self.config['name'] = info
-        end
-      end
-    end
+    attributes.concat([:config => [:location, :units]])
   end
 end
--- lib/concerto_weather/engine.rb
+++ lib/concerto_weather/engine.rb
@@ -6,5 +6,13 @@ module ConcertoWeather
       app.config.content_types << Weather
     end
 
+    def plugin_info(plugin_info_class)
+      @plugin_info ||= plugin_info_class.new do 
+        add_config("open_weather_map_api_key", "",
+                   value_type: "string",
+                   category: "API Keys",
+                   description: "OpenWeatherMap API Access Token. This token is used for obtaining weather information when adding weather content. http://openweathermap.org/appid")
+      end
+    end
   end
 end
