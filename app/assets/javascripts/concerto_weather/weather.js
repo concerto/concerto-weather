d34fd474321add58b2f2b019f24655b380561f98
Gabe Perez <perez283@gmail.com>
2016-05-07 14:29:08 -0400

    Use OpenWeatherAPI city search to return possible cities based on query

---
+++ app/assets/javascripts/concerto_weather/weather.js
@@ -0,0 +1,61 @@
+function buildResultsTable(data) {
+  // Build a table to display city query results
+  // User can select a city that best matches their intended location
+  table = "<table class='table table-condensed'> \
+           <thead><tr><th>Name</th><th>Country</th><th>Lat</th><th>Lng</th>";
+  tableBody = "<tbody>";
+
+  var places = data.list;
+  for (var i = 0; i < places.length; i++) {
+    var lat = places[i].coord.lat;
+    var lng = places[i].coord.lon;
+    var row = "<tr class='link-hl' data-lat='"+lat+"' data-lng='"+lng+"'> \
+              <td>" + places[i].name + "</td> \
+              <td>" + places[i].sys.country + "</td> \
+              <td>" + lat + "</td> \
+              <td>" + lng + "</td>";
+    tableBody += row;
+  }
+
+  tableBody += "</tbody></table>";
+  return table + tableBody;
+}
+
+function initCityIdSearch() {
+  var query_el = $('input#weather_config_city_query')
+  query_el.on('focusout', searchForCityInfo);
+
+  function searchForCityInfo() {
+    var info_el = $('.city-info');
+    var cityQuery = $('input#weather_config_city_id').val();
+
+    if (info_el.length != 0) {
+      // Add a 'searching' placeholder while city query results are loading
+      $(info_el).empty().html('<i class=\"fa fa-spinner fa-spin\"></i> searching...');
+      // Query the OpenWeatherAPI to find a list of matching cities based on input
+      $.ajax({
+        url: "/concerto_weather/city_search.js",
+        data: {"q": query_el.val()},
+        dataType: 'json',
+        timeout: 4000,
+        success: function(data) {
+          // Build a table of results returned from city query
+          var resultsTable = buildResultsTable(data);
+          // Show city results from query
+          $(info_el).empty().html(resultsTable);
+          // Handle click events on city results
+          $(info_el).find('tr').on('click', function(e) {
+            $('#weather_config_lat').val($(e.currentTarget).data("lat"));
+            $('#weather_config_lng').val($(e.currentTarget).data("lng"));
+            $(info_el).empty();
+          });
+        },
+        error: function()  {
+          $(info_el).empty().html("<p>No results found.</p>");
+        }
+      });
+    }
+  }
+}
+
+$(document).ready(initCityIdSearch);
--- lib/concerto_weather/engine.rb
+++ lib/concerto_weather/engine.rb
@@ -5,24 +5,26 @@ module ConcertoWeather
     initializer "register content type" do |app|
       app.config.content_types << Weather
     end
-    
+
     def plugin_info(plugin_info_class)
-      @plugin_info ||= plugin_info_class.new do 
+      @plugin_info ||= plugin_info_class.new do
+        add_route("concerto_weather", ConcertoWeather::Engine)
+
         add_config("open_weather_map_api_key", "",
                    value_type: "string",
                    category: "API Keys",
                    description: "OpenWeatherMap API Access Token. This token is used for obtaining weather information when adding weather content. http://openweathermap.org/appid")
-      
+
         # Add owfont (open weather) icon set to application layout for content preview / browsing
         add_header_tags do
           stylesheet_link_tag "concerto_weather/application"
         end
 
         # Add owfont (open weather) icon set to concerto-frontend layout for weather content shown on screens
-        add_view_hook "frontend/ScreensController", :concerto_frontend_plugins do 
+        add_view_hook "frontend/ScreensController", :concerto_frontend_plugins do
           "#{ stylesheet_link_tag 'concerto_weather/application' }"
         end
-        
+
       end
     end
   end
