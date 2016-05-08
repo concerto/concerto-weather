1c2b39afb4eea27e4049f38ddc6e2de3e96a5ad9
Gabe Perez <perez283@gmail.com>
2016-05-07 20:50:43 -0400

    Allow user to change selected row instead of hiding the results table

--- app/assets/javascripts/concerto_weather/weather.js
+++ app/assets/javascripts/concerto_weather/weather.js
@@ -1,20 +1,19 @@
 function buildResultsTable(data) {
-  console.log(data);
   // Build a table to display city query results
   // User can select a city that best matches their intended location
   table = "<table class='table table-condensed'> \
-           <thead><tr><th>Name</th><th>Country</th><th>Lat</th><th>Lng</th>";
+           <thead><th>Name</th><th>Country</th><th>Lat</th><th>Lng</th>";
   tableBody = "<tbody>";
 
   var places = data.list;
   for (var i = 0; i < places.length; i++) {
     var lat = places[i].coord.lat;
     var lng = places[i].coord.lon;
-    var row = "<tr class='link-hl' data-lat='"+lat+"' data-lng='"+lng+"'> \
-              <td>" + places[i].name + "</td> \
-              <td>" + places[i].sys.country + "</td> \
-              <td>" + lat + "</td> \
-              <td>" + lng + "</td>";
+    var row = "<tr class='link-hl' data-selected='0' data-lat='"+lat+"' data-lng='"+lng+"'> \
+               <td>" + places[i].name + "</td> \
+               <td>" + places[i].sys.country + "</td> \
+               <td>" + lat + "</td> \
+               <td>" + lng + "</td>";
     tableBody += row;
   }
 
@@ -24,21 +23,21 @@ function buildResultsTable(data) {
 }
 
 function initCityIdSearch() {
-  var query_el = $('input#weather_config_city_query')
-  query_el.on('focusout', searchForCityInfo);
+  var query_el = $("input#weather_config_city_query")
+  query_el.on("focusout", searchForCityInfo);
 
   function searchForCityInfo() {
-    var info_el = $('.city-info');
-    var cityQuery = $('input#weather_config_city_id').val();
+    var info_el = $(".city-info");
+    var cityQuery = $("input#weather_config_city_query").val();
 
-    if (info_el.length != 0) {
+    if (info_el.length != 0 && cityQuery.length > 0) {
       // Add a 'searching' placeholder while city query results are loading
-      $(info_el).empty().html('<i class=\"fa fa-spinner fa-spin\"></i> searching...');
+      $(info_el).empty().html("<i class='fa fa-spinner fa-spin'></i> searching...");
       // Query the OpenWeatherAPI to find a list of matching cities based on input
       $.ajax({
         url: "/concerto_weather/city_search.js",
-        data: {"q": query_el.val()},
-        dataType: 'json',
+        data: {"q": cityQuery},
+        dataType: "json",
         timeout: 4000,
         success: function(data) {
           // Build a table of results returned from city query
@@ -46,10 +45,18 @@ function initCityIdSearch() {
           // Show city results from query
           $(info_el).empty().html(resultsTable);
           // Handle click events on city results
-          $(info_el).find('tr').on('click', function(e) {
-            $('#weather_config_lat').val($(e.currentTarget).data("lat"));
-            $('#weather_config_lng').val($(e.currentTarget).data("lng"));
-            $(info_el).empty();
+          $(info_el).find("tr").on("click", function(e) {
+            // Lat/lng saved with weather content for future API calls.
+            $("#weather_config_lat").val($(this).attr("data-lat"));
+            $("#weather_config_lng").val($(this).attr("data-lng"));
+            // Reset all selected rows
+            $(info_el).find("tr").each(function() {
+              $(this).attr("data-selected", "0");
+              $(this).removeClass("alert-info");
+            });
+            // Set a new selected row
+            $(this).attr("data-selected", "1");
+            $(this).addClass("alert-info");
           });
         },
         error: function()  {
