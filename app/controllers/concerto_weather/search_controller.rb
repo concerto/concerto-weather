7d973b7d22e8bcf1aa75989c1923f4aaa87ad298
Diego Garcia del Rio <garci66@gmail.com>
2017-03-02 00:45:23 -0300

    minor variable renaming as per PR comments

--- app/controllers/concerto_weather/search_controller.rb
+++ app/controllers/concerto_weather/search_controller.rb
@@ -14,9 +14,9 @@ module ConcertoWeather
         require 'json'
         require 'erb'
 
-        query_url=ERB::Util.url_encode(query)
+        query_escaped=ERB::Util.url_encode(query)
         appid = ConcertoConfig["open_weather_map_api_key"]
-        url = "http://api.openweathermap.org/data/2.5/find?q=#{query_url}&type=like&mode=json&appid=#{appid}"
+        url = "http://api.openweathermap.org/data/2.5/find?q=#{query_escaped}&type=like&mode=json&appid=#{appid}"
         return Net::HTTP.get(URI(url))
       end
   end
